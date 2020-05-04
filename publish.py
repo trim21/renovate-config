a7a06ed1d074fd84adf4c641d89d05613ed67550
Trim21 <i@trim21.me>
2020-05-04 13:41:11 +0800

    up

---
+++ publish.py
@@ -0,0 +1,25 @@
+import json
+import os
+import os.path
+
+
+def main():
+    with open("src/package.json", encoding="utf8") as f:
+        pkg = json.load(f)
+    files = []
+    for file in os.listdir("./"):
+        if file.endswith(".json"):
+            files.append(file)
+
+    pkg["renovate-config"] = {fs.split(".")[0]: load_json(fs) for fs in files}
+    with open("package.json", "w+", encoding="utf-8") as f:
+        json.dump(pkg, f, indent=2, ensure_ascii=False)
+
+
+def load_json(fs) -> dict:
+    with open(fs, encoding="utf-8") as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    main()
