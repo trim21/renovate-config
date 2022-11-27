57cc5b74d37244aa74ccfb00ae1079a58391599f
Trim21 <trim21.me@gmail.com>
2022-11-28 07:51:53 +0800

    test

--- test.mjs
+++ test.mjs
@@ -5,10 +5,11 @@ import * as fs from "fs";
 
 const files = fs.readdirSync(".");
 
-files.filter(x => x.endsWith(".json")).forEach(x => {
+files.filter(x => x !== "package.json" && x.endsWith(".json")).forEach(x => {
   cp.execSync("npx renovate-config-validator", {
     stdio: "inherit",
     env: {
+      PATH: process.env.PATH ?? process.env.Path,
       RENOVATE_CONFIG_FILE: path.resolve(".", x),
     },
   });
